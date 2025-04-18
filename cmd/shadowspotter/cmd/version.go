3601c96df256a6b74ed98db3a1e2c5c1c2e39576
Weidsom Nascimento <source@snakesecurity.org>
2025-04-18 12:38:50 -0700

    Update version and add LICENSE

--- cmd/shadowspotter/cmd/version.go
+++ cmd/shadowspotter/cmd/version.go
@@ -1,3 +1,23 @@
+/*
+Shadow-Spotter Next Gen Content Discovery
+Copyright (C) 2024  Weidsom Nascimento - SNAKE Security
+
+Based on kiterunner from AssetNote
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 package cmd
 
 import (
@@ -9,9 +29,8 @@ import (
 // These global variables are injected at build time to provide the
 // version command
 var (
-	Version = "v0.0.1"
-	Commit  = "commit"
-	Date    = "today"
+	Version = "v0.0.2"
+	Edition  = "ANDRAX-NG"
 )
 
 // versionCmd represents the version command
@@ -20,8 +39,7 @@ var versionCmd = &cobra.Command{
 	Short: "version of the binary you're running",
 	Long: "\n\n\033[1;97mCopyright\033[0m \033[1;92m©\033[0m \033[1;96m2024\033[0m \033[1;92mSNAKE Security\033[0m \033[1;97m-\033[0m \033[1;91mJust one bite\033[0m\n\n" + `this shows you the version of the binary that is running`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("%s - %s\n", Version, Commit)
-		fmt.Printf("Built on %s\n", Date)
+		fmt.Printf("%s - %s\n", Version, Edition)
 	},
 }
 
--- pkg/http/doc.go
+++ pkg/http/doc.go
@@ -1,4 +1,24 @@
 /*
+Shadow-Spotter Next Gen Content Discovery
+Copyright (C) 2024  Weidsom Nascimento - SNAKE Security
+
+Based on kiterunner from AssetNote
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+/*
 Package http provides a 0 allocation wrapper around the fasthttp library.
 
 The wrapper facilitates our redirect handling, our custom "target" and request building format,
